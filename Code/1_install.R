5eeb61c340ec365c86f752dec9da50d8e64051c8
greenmanta <ibnu.ariyanto07@gmail.com>
2021-12-24 21:33:39 +0700

    Commit

---
+++ Code/1_install.R
@@ -0,0 +1,14 @@
+#install package
+install.packages("devtools")
+devtools::install_git
+hub("leonjessen/PepTools")
+                 
+
+
+
+
+library(keras)
+library(tidyverse)
+library(PepTools)
+library(janitor)
+set.seed(1458) 
